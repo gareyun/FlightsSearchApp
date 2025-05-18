21191784bd22a93d79a799a38f4a3e306e55e355
gareyun <az908az908@bk.ru>
2025-05-18 16:46:56 +0400

    for pull request

--- app/src/main/java/com/example/flightsearchapp/MainActivity.kt
+++ app/src/main/java/com/example/flightsearchapp/MainActivity.kt
@@ -13,7 +13,6 @@ import com.example.flightsearchapp.data.repository.FlightSearchRepository
 import com.example.flightsearchapp.ui.theme.FlightSearchAppTheme
 import com.example.flightsearchapp.ui.viewmodels.FlightSearchViewModel
 
-
 class MainActivity : ComponentActivity() {
     private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name="search_preferences")
 
